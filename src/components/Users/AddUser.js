a9ed9599ca90badc1f502f1076445493429c579d
Yashraj Shukla <yashrajshukla48@gmail.com>
2023-03-26 21:53:02 +0530

    adding card component to the form

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,15 +1,18 @@
+import Card from "../UI/Card";
 const AddUser = (props) => {
   const addUserHandler = (event) => {
     event.preventDefault();
   };
   return (
-    <form onSubmit={addUserHandler}>
-      <label htmlFor="username"> Username</label>
-      <input type="text" id="username" />
-      <label htmlFor="age"> Work Experience (years)</label>
-      <input type="number" id="age" />
-      <button type="submit">Apply for Co-Founder!</button>
-    </form>
+    <Card>
+      <form onSubmit={addUserHandler}>
+        <label htmlFor="username"> Username</label>
+        <input type="text" id="username" />
+        <label htmlFor="age"> Work Experience (years)</label>
+        <input type="number" id="age" />
+        <button type="submit">Apply for Co-Founder!</button>
+      </form>
+    </Card>
   );
 };
 export default AddUser;
